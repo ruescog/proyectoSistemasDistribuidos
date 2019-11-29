ad38a08017e2892bdc86afc9b37a19b921fdc635
ruescog <ruescog@CCTL132-06.unirioja.loc>
2019-11-29 16:40:26 +0100

    Problemas de descarga solucionados en el descargador. Si intentaba bajarse algunos archivos se petaba y se paraba, ahora intenta bajar lo que pueda hasta terminar.

--- proyectoSistemasDistribuidos/src/logicaDelPrograma/Descargador.java
+++ proyectoSistemasDistribuidos/src/logicaDelPrograma/Descargador.java
@@ -15,12 +15,16 @@ import logicaDeDatos.Replicador;
 public class Descargador {
 
 	private File fichero;
+	private int urlInspeccionadas;
+	private int archivosDescargados;
 
 	public Descargador(File fichero) {
 		// CADA COMPONENTE DESCARGADOR ESTÁ COMPUESTA POR EL FICHERO DEL CUAL VA A
 		// DESCARGAR SUS LINEAS
 
 		this.fichero = fichero;
+		this.urlInspeccionadas = 0;
+		this.archivosDescargados = 0;
 	}
 
 	public void descargar() {
@@ -38,61 +42,76 @@ public class Descargador {
 			while ((img = fis.readLine()) != null) {
 
 				// SUPONE QUE ES UNA URL A UN ARCHIVO QUE ESTÁ EN LA NUBE
-				url = new URL(img);
+				try {
+					url = new URL(img);
 
-				// CREA LOS OBJETOS NECESARIOS PARA LEER DE LA WEB Y GUARDARLO EN LOCAL, NOMBRA
-				// AL OBJETO CON EL NOMBRE QUE TENGA EN LA WEB
-				file = url.getFile();
+					// CREA LOS OBJETOS NECESARIOS PARA LEER DE LA WEB Y GUARDARLO EN LOCAL, NOMBRA
+					// AL OBJETO CON EL NOMBRE QUE TENGA EN LA WEB
+					file = url.getFile();
 
-				// DETERMINA DONDE ESTARÁ EL NOMBRE DEL FICHERO
+					// DETERMINA DONDE ESTARÁ EL NOMBRE DEL FICHERO
 
-				indiceBarra = file.lastIndexOf("/") + 1; // +1 PORQUE NO NOS INTERESA LA BARRA ... NOTAR QUE EN CASO DE
-															// QUE SEA 0 (NO HAYA BARRA), ESTO INDICARÁ AL PRINCIPIO DE
-															// LA CADENA
-				indiceInterrogante = file.indexOf("?"); // LA MAYORÍA DE LOS ARCHIVOS TENDRÁN INFORMACIÓN ADICIONAL
-														// SEPARADA POR EL ?, NO NOS INTERESA AQUI.
+					// +1 PORQUE NO NOS INTERESA LA BARRA ... NOTAR QUE EN CASO DE QUE SEA 0 (NO
+					// HAYA BARRA), ESTO INDICARÁ AL PRINCIPIO DE LA CADENA
+					indiceBarra = file.lastIndexOf("/") + 1;
 
-				if (indiceInterrogante == -1) {
-					indiceInterrogante = file.length(); // ESTE INDICE NO SE "ARREGLA SOLO" COMO EL DE /, ASÍ QUE SI NO
-														// HAY INTERROGANTE TENEMOS QUE INDICAR QUE TOMARÁ COMO NOMBRE
-														// TODA LA CADENA
-				}
+					// LA MAYORÍA DE LOS ARCHIVOS TENDRÁN INFORMACIÓN ADICIONAL SEPARADA POR EL ?,
+					// NO NOS INTERESA AQUI.
+					indiceInterrogante = file.indexOf("?");
 
-				file = file.substring(indiceBarra, indiceInterrogante); // EL NOMBRE DE FICHERO (QUE SE GENERARÁ)
-				multiplicidad = Replicador.añadirDato(file);
-				if(multiplicidad != 0) { //SI ESTÁ REPETIDO ...
-					formato = file.substring(file.lastIndexOf("."));
-					file = file.substring(0, file.lastIndexOf("."));
-					file = file.concat(" (" + multiplicidad + ")");
-					file = file.concat(formato);
-					
-				}
+					// ESTE INDICE NO SE "ARREGLA SOLO" COMO EL DE /, ASÍ QUE SI NO HAY INTERROGANTE
+					// TENEMOS QUE INDICAR QUE TOMARÁ COMO NOMBRE TODA LA CADENA
+					if (indiceInterrogante == -1) {
+						indiceInterrogante = file.length();
 
-				try (InputStream in = new BufferedInputStream(url.openStream());
-						FileOutputStream fos = new FileOutputStream(file);) {
-
-					// DESCARGA TODA LA INFORMACIÓN DE LA URL QUE INDICABA LA LINEA DEL FICHERO
-					while ((leidos = in.read(buff)) > 0) {
-						fos.write(buff, 0, leidos);
 					}
 
-					fos.flush(); // DE NUEVO, UN FLUSH REDUNDANTE POR EL TRY WITH RESOURCES
+					file = file.substring(indiceBarra, indiceInterrogante); // EL NOMBRE DE FICHERO (QUE SE GENERARÁ)
+					multiplicidad = Replicador.añadirDato(file);
+					if (multiplicidad != 0) { // SI ESTÁ REPETIDO ...
+						formato = file.substring(file.lastIndexOf("."));
+						file = file.substring(0, file.lastIndexOf("."));
+						file = file.concat(" (" + multiplicidad + ")");
+						file = file.concat(formato);
 
-				} catch (IOException e) {
+					}
 
-					// MENSAJE DE ERROR EN CASO DE QUE HAYA UN PROBLEMA CON LA APERTURA DE LOS
-					// FICHEROS O DEL URL.OPENSTREAM.
-					System.err.println("Ha ocurrido un error inesperado.");
+					try (InputStream in = new BufferedInputStream(url.openStream());
+							FileOutputStream fos = new FileOutputStream(file);) {
+
+						// DESCARGA TODA LA INFORMACIÓN DE LA URL QUE INDICABA LA LINEA DEL FICHERO
+						while ((leidos = in.read(buff)) > 0) {
+							fos.write(buff, 0, leidos);
+						}
+
+						fos.flush(); // DE NUEVO, UN FLUSH REDUNDANTE POR EL TRY WITH RESOURCES
+						
+						// +1 A ARCHIVOS DESCARGADOS
+						this.archivosDescargados++;
+
+					} catch (IOException e) {
+
+						// MENSAJE DE ERROR EN CASO DE QUE HAYA UN PROBLEMA CON LA APERTURA DE LOS
+						// FICHEROS O DEL URL.OPENSTREAM.
+						System.err.println("Ha ocurrido un error inesperado.");
+					}
+					catch (IllegalArgumentException e) {
+						System.err.println("El protocolo o el host son desconocidos.");
+					}
+				} catch (MalformedURLException e) {
+					// MENSAJE DE ERROR EN CASO DE QUE LA URL NO CUMPLA CON LA SINTAXIS (NO EXISTA)
+					System.err.println("La URL especificada no ha podido ser encontrada.");
+				}
+				finally {
+					this.urlInspeccionadas++;
 				}
 			}
 
-			// MENSAJE DE ÉXTIO
+			// MENSAJE DE INFORMACION
+			System.out.print("Porcentaje de éxito de descarga: "
+					+ this.archivosDescargados * 100.0 / this.urlInspeccionadas + "%.");
+			System.out.println(" Descargado: " + this.archivosDescargados + ", inspeccionados: " + this.urlInspeccionadas);
 			System.out.println("Descarga de imagenes terminada.");
-
-		} catch (MalformedURLException e) {
-			// MENSAJE DE ERROR EN CASO DE QUE LA URL NO CUMPLA CON LA SINTAXIS (NO EXISTA)
-			System.err.println("La URL especificada no ha podido ser encontrada.");
-
 		} catch (IOException ex1) {
 
 			// MENSAJE DE ERROR EN CASO DE QUE HAYA UN PROBLEMA CON LA APERTURA DE LOS
